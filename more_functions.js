f8ae02c9659fb8891e47d43dfccbad74bf5440e7
Salem H <salemkassal@gmail.com>
2024-10-15 15:08:32 -0400

    exercise2

--- more_functions.js
+++ more_functions.js
@@ -1,3 +1,30 @@
-function displayMailinglable(){
-    
+function displayMailingLabel(name, address, city, state, zip){
+   console.log(name);
+   console.log(address);
+   console.log(city);
+   console.log(state);
+   console.log(zip);
 }
+
+displayMailingLabel("Salem", "1036 Westmoreland", "Atlanta", "GA", "30043")
+
+function addNumbers(num1, num2){
+  const add = num1 + num2;
+//   console.log("num1 + num2 = sum")
+  console.log(`${num1} + ${num2} = ${add}`)
+}
+
+addNumbers(5,10);
+addNumbers(25,10);
+
+function displayReceipt(totalDue, amountPaid){
+    const changeDue = totalDue - amountPaid;
+    console.log(`${totalDue} - ${amountPaid} = ${changeDue}`)
+    if(amountPaid > totalDue){
+        console.log("You need to pay more!")
+    }
+}
+
+displayReceipt(50,46);
+displayReceipt(46,46);
+displayReceipt(45,46);
