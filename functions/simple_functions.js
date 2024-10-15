0a712b5d2062029dab5555744d905827eca81067
Salem H <salemkassal@gmail.com>
2024-10-15 14:29:55 -0400

    exercise1

---
+++ functions/simple_functions.js
@@ -0,0 +1,19 @@
+function favoriteThings() {
+    console.log("Name: Salem")
+    console.log("Favorite movie : The Age of Adaline")
+    console.log("Favorite musician : Dawit")
+}
+
+favoriteThings();
+
+function whyImHere(){
+    console.log("I have joined Learn to Code Acadamy because i want to build innovative software solution by appling my analytical skills and creativity!")
+}
+
+whyImHere();
+
+function favoritePlace(){
+    console.log("I want to visit Europe because i love exploring old arcitecture and arts")
+}
+
+favoritePlace();
